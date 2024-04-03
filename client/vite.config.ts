09090cc0954dca8871750b9c77b50ce3ef2f0c21
Edanriell <Edanriell@gmail.com>
2024-04-03 14:10:21 +0300

    given shape to app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,11 +1,11 @@
-import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from "path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import VueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
+const root = resolve(__dirname, "src");
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,7 +14,17 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      "@": resolve(root),
+      "@app": resolve(root, "app"),
+      "@entities": resolve(root, "entities"),
+      "@entities/recipe": resolve(root, "entities/recipe"),
+      "@entities/recipe/api": resolve(root, "entities/recipe/api"),
+      "@entities/recipe/model": resolve(root, "entities/recipe/model"),
+      "@entities/recipe/ui": resolve(root, "entities/recipe/ui"),
+      "@shared": resolve(root, "shared"),
+      "@shared/ui": resolve(root, "shared/ui"),
+      "@shared/ui/button": resolve(root, "shared/ui/button"),
+      "@shared/ui/icon": resolve(root, "shared/ui/icon")
     }
   }
 })
