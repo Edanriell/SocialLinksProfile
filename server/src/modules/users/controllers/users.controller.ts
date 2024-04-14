12e6ff2ae48fe552b8db222aeac0db3fdaa92697
Edanriell <Edanriell@gmail.com>
2024-04-14 21:08:03 +0300

    implemented a logger

--- server/src/modules/users/controllers/users.controller.ts
+++ server/src/modules/users/controllers/users.controller.ts
@@ -1,14 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Ip } from "@nestjs/common";
 import { UsersService } from "../services";
 import { Throttle } from "@nestjs/throttler";
+import { LoggerService } from "../../logger/logger.service";
 
 @Controller()
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
+	private readonly logger = new LoggerService(UsersController.name);
 
 	@Throttle({ short: { ttl: 1000, limit: 1 } })
 	@Get("user")
-	getHello(): string {
+	getHello(@Ip() ip: string): string {
+		this.logger.fatal(`Request for ALL Employees\t${ip}`, UsersController.name);
 		return this.usersService.getUser();
 	}
 }
