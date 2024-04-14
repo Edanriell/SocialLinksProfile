224cb7e47abc3b60e1df881438fa34723d690772
Edanriell <Edanriell@gmail.com>
2024-04-14 19:31:44 +0300

    imlemented throttle

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,20 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { EnvConfigModule, RouterConfigModule } from "./config";
+import { EnvConfigModule, RouterConfigModule, throttleConfig } from "./config";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-	imports: [EnvConfigModule, RouterConfigModule],
+	imports: [EnvConfigModule, RouterConfigModule, ThrottlerModule.forRoot(throttleConfig)],
 	controllers: [AppController],
-	providers: [AppService]
+	providers: [
+		AppService,
+		{
+			provide: APP_GUARD,
+			useClass: ThrottlerGuard
+		}
+	]
 })
 export class AppModule {}
 
--- server/src/modules/users/controllers/users.controller.ts
+++ server/src/modules/users/controllers/users.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get } from "@nestjs/common";
 import { UsersService } from "../services";
+import { Throttle } from "@nestjs/throttler";
 
 @Controller()
 export class UsersController {
 	constructor(private readonly usersService: UsersService) {}
 
+	@Throttle({ short: { ttl: 1000, limit: 1 } })
 	@Get("user")
 	getHello(): string {
 		return this.usersService.getUser();
