7e92892c9f9bc19f3ef428506c2665689cafad76
Edanriell <Edanriell@gmail.com>
2024-04-16 21:13:57 +0300

    started to work on endpoints

--- server/src/modules/users/services/users.service.ts
+++ server/src/modules/users/services/users.service.ts
@@ -1,30 +1,33 @@
 import { Model } from "mongoose";
-import { Inject, Injectable } from "@nestjs/common";
+import { Inject, Injectable, Ip } from "@nestjs/common";
 
 import { User } from "../models";
 import { UserDto } from "../dtos";
+import { UsersController } from "../controllers";
+import { LoggerService } from "../../logger/services";
 
 @Injectable()
 export class UsersService {
+	private readonly logger = new LoggerService(UsersController.name);
+
 	constructor(
 		@Inject("USER_MODEL")
-		private catModel: Model<User>
+		private userModel: Model<User>
 	) {}
 
-	getUser(): string {
+	getUser(@Ip() ip?: string): string {
+		this.logger.warn(`Request for ALL Employees\t${ip}`, UsersController.name);
 		return "Test";
 	}
 
 	async createUser(createCatDto: UserDto): Promise<User> {
-		const createdCat = new this.catModel(createCatDto);
+		const createdCat = new this.userModel(createCatDto);
 		return createdCat.save();
 	}
 
 	async seedUsersData() {}
 
 	async findAll(): Promise<User[]> {
-		return this.catModel.find().exec();
+		return this.userModel.find().exec();
 	}
 }
-
-// DUMMY METHODS
