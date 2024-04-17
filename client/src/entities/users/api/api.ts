dee6b40aadaf853580b0e4e939c8467433666f39
Edanriell <Edanriell@gmail.com>
2024-04-17 19:54:35 +0300

    finished feature

--- client/src/entities/users/api/api.ts
+++ client/src/entities/users/api/api.ts
@@ -1,5 +1,5 @@
 export const getUserQuery = async () => {
-	const response = await fetch(import.meta.env.VITE_API_URL! + "api/getrecipe", {
+	const response = await fetch(import.meta.env.VITE_API_URL! + "api/user", {
 		method: "Get"
 	});
 
